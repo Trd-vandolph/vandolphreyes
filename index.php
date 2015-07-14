7a9b265ad29fcc7cb4fabd02930e353f365ddb03
Trd-vandolph <trd.vandolph@gmail.com>
2015-07-14 19:42:45 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+
+  	<meta name="keywords" content="OliveCode,Online Computer Programming Course,Job Guaranteed,WordPress,HTML,CSS,Programming,Git,JavaScript,Bangladesh,Private Lesson,Edoo" />
+  	<meta name="description" content="OliveCode is the job guaranteed online computer programming course for Bangladeshi. It is a private lesson in English for 4 months, followed by 2-month internship at an IT company." />
+
+	<link rel="shortcut icon" href="assets/img/me.ico">
+	<meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+
+	<meta property="og:title" content="Olive code | Job Guaranteed Online Computer Programming Course for Bangladeshi">
+	<meta property="og:description" content="OliveCode is the job guaranteed online computer programming course for Bangladeshi. It is a private lesson in English for 4 months, followed by 2-month internship at an IT company.">
+	<meta property="og:url" content="http://www.olivecode.com/">
+	<meta property="og:image" content="http://www.olivecode.com/assets/img/front/logo_front.png?1423213936">
+	<meta property="og:site_name" content="OliveCode">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,700,600,500,800,900,300,100,200' rel='stylesheet' type='text/css'>
+
+	<title>Vandolph Reyes | Vandolph Capacio Reyes Personal Website / Portfolio</title>
+</head>
+<body>
+<header>
+	<nav>
+
+	</nav>
+</header>
+<section id="signup-button">
+
+</section>
+
+<footer>
+	<div class="fb-like-page">
+		<div>
+			<a href="http://www.facebook.com/olivecode" target="_blank"><img src="/assets/img/front/facebook-label.jpg" alt="facebook-label"></a>
+			<div class="fb-page" data-href="https://www.facebook.com/olivecode" data-width="500" data-hide-cover="false" data-show-facepile="false" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/olivecode"><a href="https://www.facebook.com/olivecode">OliveCode</a></blockquote></div></div>
+		</div>
+	</div>
+
+	<nav>
+		<ul>
+			<li><a href="/students/contactforum">Contact Us</a></li>
+
+			<li><a href="/contact">Contact Us</a></li>
+
+			<li><a href="/privacy">Privacy Policy</a></li>
+			<li><a href="/terms">Agreement</a></li>
+			<li><a href="http://www.edoo.co.jp/" class="blank">Operating Company</a></li>
+		</ul>
+	</nav>
+	<p class="copyright">© 2015 Edoo Inc. All rights reserved.</p>
+</footer>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+</body>
+</html>
+
